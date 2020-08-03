6b44947e9d5e14ee42147b79f7e71998f974f8c0
tandelDipak <dipak.tandel96@gmail.com>
2020-08-03 15:11:35 +0530

    Fixed circular import issue

--- taskApp/app.py
+++ taskApp/app.py
@@ -1,28 +1,4 @@
 import dash
-import os
-from layouts import layout1
-import config
-
-
-environment = os.environ['CONF_ENV']
-if environment == 'production':
-    print("Production Configuration")
-    appConfig = config.ProductionConfig()
-elif environment == 'development':
-    print("Development Configuration")
-    appConfig = config.DevelopmentConfig()
-elif environment == 'testing':
-    print("Testing Configuration")
-    appConfig = config.TestingConfig()
-elif environment == 'jenkinsDevelopment':
-    print("Jenkins Development Configuration")
-    appConfig = config.JenkinsDevelopmentConfig()
-elif environment == 'jenkinsTesting':
-    print("Jenkins Testing Configuration")
-    appConfig == config.JenkinsTestingConfig()
-else:
-    appConfig = config.config()
-    print("Default Configuration")
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -30,6 +6,3 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.layout = layout1
-if __name__ == '__main__':
-    app.run_server(port=appConfig.APP_PORT, debug=appConfig.DEBUG)
