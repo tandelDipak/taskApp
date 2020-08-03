a656f321ef22751b0773b85dd4f9d835c4c30905
tandelDipak <dipak.tandel96@gmail.com>
2020-08-03 15:12:26 +0530

    Added function docstring, Fixed title Text

--- taskApp/callbacks.py
+++ taskApp/callbacks.py
@@ -6,11 +6,25 @@ import constants as C
 from app import app
 
 
+TITLE_STYLE = C.TITLE_STYLE
+
+
 @app.callback([
     dash.dependencies.Output('graph3', 'figure'),
     dash.dependencies.Output('graph2', 'figure')],
     [dash.dependencies.Input('graph1', 'clickData')])
 def updateOnClick(clickValue):
+    """ Generate figure based on click event data
+
+    Args:
+        clickValue (dict): Data from latest click event
+
+    Raises:
+        dash.exceptions.PreventUpdate: Prevent updates when API is not reachable
+
+    Returns:
+        Figure : Plotly Figure Object
+    """
     try:
         print(clickValue)
         isFemale = clickValue['points'][0]['curveNumber']
@@ -24,34 +38,44 @@ def updateOnClick(clickValue):
     else:
         apiUrl = C.API_BASE_URL + "graph2/" + ageRange + "/0"
     print(apiUrl)
-    response = requests.get(apiUrl)
+
+    # Get the data, if api not reachable prevent update
+    try:
+        response = requests.get(apiUrl)
+    except requests.exceptions.RequestException as e:
+        print(e)
+        raise dash.exceptions.PreventUpdate
+
+    TITLE_STYLE['text'] = 'Fare Distribution'
     scatterPlot = go.Figure(
         data=[go.Scatter(
             x=response.json()['x'],
             y=response.json()['y'],
             mode='markers',
+            # TODO: Marker color based on Pclass
             # marker_color=response.json()['color']
             # marker={'color': response.json()['color'], 'colorscale':[
             #    [0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']]}
         )],
         layout=go.Layout(
-            title=C.TITLE_STYLE,
+            title=TITLE_STYLE,
             font=C.FONT_STYLE,
             xaxis_title="Passanger ID",
             yaxis_title="Fare"
 
         )
     )
+
+    TITLE_STYLE['text'] = 'Passanger Class Distribution'
     piePlot = go.Figure(
         data=[go.Pie(
             labels=response.json()['labels'],
             values=response.json()['values']
         )],
         layout=go.Layout(
-            title=C.TITLE_STYLE,
+            title=TITLE_STYLE,
             font=C.FONT_STYLE,
             legend_title="Passanger Class",
         )
     )
-
     return piePlot, scatterPlot
